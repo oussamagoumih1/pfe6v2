904d21fff219431f99ee389e8c859993a2c79d20
mohamedkabbaj99 <mohamedkabbaj99>
2021-09-08 03:39:07 +0200

    edit by simo

--- src/main/java/com/example/testpfe/ws/provided/admin/ProduitCategorieProvidedRest.java
+++ src/main/java/com/example/testpfe/ws/provided/admin/ProduitCategorieProvidedRest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.example.testpfe.ws.provided;
+package com.example.testpfe.ws.provided.admin;
 
 import com.example.testpfe.bean.ProduitCategorie;
 import com.example.testpfe.service.facade.ProduitCategorieService;
